aff456367ac6e1a49b0c315f66230aa02cb551c2
sunchaser <anika.mclean@gmail.com>
2022-05-08 21:20:50 -0400

    set up file

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+require('dotenv').config();
+console.log(process.env.password)
+// Connect to database
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: process.env.user,
+  // Your MySQL password
+  password: process.env.password,
+  database: 'employee'
+});
+
+module.exports = db;
