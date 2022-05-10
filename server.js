e4da2a0bfdbd0df83fb4f63b0b99b912556476b5
sunchaser <anika.mclean@gmail.com>
2022-05-09 23:54:23 -0400

    update server.js

--- server.js
+++ server.js
@@ -12,19 +12,70 @@ connection.connect((err) => {
 const dbMain = () => {
   inquirer.prompt([
     {
-      name: 'choices',
+      name: 'input',
       type: 'list',
       message: 'Welcome to CBC employee database. What would you like to do?',
       choices: [
         'View all departments',
         'View all roles',
         'View all employees',
-        'Add a department',
-        'Add a role',
+        'View employees by department',
+        'View department budget',
+        'Add department',
+        'Add role',
         'Add an employee',
         'Update an employee role',
+        'Update employee managers',
+        'Delete department',
+        'Delete role',
+        'Delete employee',
         'Exit'
       ]
     }
   ])
-}
+    .then(({ input }) => {
+      switch (input) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'View employees by department': viewEmployeesByDepartment();
+          break;
+        case 'View department budget':
+          viewDepartmentBudget();
+          break;
+        case 'Add department':
+          addDepartment();
+          break;
+        case 'Add role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Update employee managers':
+          updateEmployeeManager();
+          break;
+        case 'Delete department':
+          deleteDepartment();
+          break;
+        case 'Delete role':
+          deleteRole();
+          break;
+        case 'Delete employee':
+          deleteEmployee();
+          break;
+        case 'Exit':
+          exitApp();
+          break;
+      };
+    }); 
+};    
