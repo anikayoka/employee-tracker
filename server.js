452776b6cdbbfd92fad249db53f5a22674060be3
sunchaser <anika.mclean@gmail.com>
2022-05-14 22:29:57 -0400

    add add queries

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cTable = require('console.table');
 //DB connect
 db.connect((err) => {
   if (err) throw err;
+  console.log('Database connected!');
   dbMain();
 });
 
@@ -32,10 +33,6 @@ const dbMain = () => {
         'Add role',
         'Add an employee',
         'Update an employee role',
-        'Update employee managers',
-        'Delete department',
-        'Delete role',
-        'Delete employee',
         'Exit'
       ]
     }
@@ -69,18 +66,6 @@ const dbMain = () => {
         case 'Update an employee role':
           updateEmployeeRole();
           break;
-        case 'Update employee managers':
-          updateEmployeeMgrs();
-          break;
-        case 'Delete department':
-          deleteDept();
-          break;
-        case 'Delete role':
-          deleteRole();
-          break;
-        case 'Delete employee':
-          deleteEmployee();
-          break;
         case 'Exit':
           exitApp();
           break;
@@ -113,27 +98,93 @@ function viewAllEmployees() {
 function viewEmployeesByDept() {
   db.query
     ("SELECT e.first_name, e.last_name, r.title, r.salary, e2.first_name, e2.last_name, d.dept_name FROM employee e LEFT JOIN roles r ON r.id = e.role_id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee e2 ON e.manager_id = e2.id ORDER BY d.dept_name", function (err, data) {
+      if (err) throw err;
+      cTable(data)
+      dbMain();
+    })
+};
+function viewDeptBudget() {
+  db.query("SELECT d.dept_name, r.salary, sum(r.salary) AS budget FROM employee e LEFT JOIN roles r ON r.id = e.role_id LEFT JOIN department d ON d.id = r.department_id ORDER BY d.dept_name, r.salary", function (err, data) {
     if (err) throw err;
     cTable(data)
     dbMain();
   })
 };
-function viewDeptBudget() {
-  db.query ("SELECT d.dept_name, r.salary, sum(r.salary) AS budget FROM employee e LEFT JOIN roles r ON r.id = e.role_id LEFT JOIN department d ON d.id = r.department_id ORDER BY d.dept_name, r.salary", function (err, data) {
-    if (err) throw err;
-    cTable(data)
-    dbMain();  
-  })
-};
 
 // ADDS
 
-function  addDept() {
+function addDept() {
   // Call inquirer first, then push database
   inquirer.prompt([
-    name: 'newDept',
-    type: 'input',
-    message: 'What department would you like to add'
-  ])
-  db.query ()
-}
+    {
+      name: 'newDept',
+      type: 'input',
+      message: 'What department would you like to add?'
+    }
+  ]).then((response) => {
+    db.query("INSERT INTO department (dept_name) VALUES (?)", [ response.newDept ], function (err, data) {
+      if (err) throw err;
+      cTable(data)
+      dbMain();
+    })
+  })
+};
+
+function addRole() {
+  inquirer.prompt([
+    {
+      name: 'newTitle',
+      type: 'input',
+      message: 'Enter role to be added'
+    },
+    {
+      name: 'newSalary',
+      type: 'input',
+      message: 'Please enter salary for this role'
+    },
+    {
+      name: 'dept_id',
+      type: 'list',
+      message: 'Please choose department for this role',
+      choices: department
+    }
+  ]).then((response) => {
+    db.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [ response.newTitle, response.newSalary, response.dept_id ], function (err, data) {
+      if (err) throw err
+      cTable(data)
+      dbMain();
+    })
+  })
+};
+
+function addEmployee() {
+  inquirer.prompt([
+    {
+      name: 'eFirst_name',
+      type: 'input',
+      message: 'Enter first name of employee added'
+    },
+    {
+      name: 'eLastName',
+      type: 'input',
+      message: 'Enter last name of employee added'
+    },
+    {
+      name: 'roleId',
+      type: 'input',
+      message: 'Enter employee role id',
+    },
+    {
+      name: 'mgrId',
+      type: 'input',
+      message: 'Enter employee manager id',
+      
+    }
+  ]).then((response) => {
+    db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [ response.eFirstNAme, response.eLastName, response.roleId, respone.mgrId ], function (err, data) {
+      if (err) throw err
+      cTable(data)
+      dbMain();
+    })
+  })
+}; 
